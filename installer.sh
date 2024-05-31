bd4512589d354ed508b30a350b53022644857368
Василий Полосухин <polosuhin7@gmail.com>
2024-05-31 18:39:03 +0500

    init

--- installer.sh
+++ installer.sh
@@ -1,6 +1,3 @@
-# htpasswd
-apt-get install apache2-utils
-
 # nvm
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
@@ -12,9 +9,11 @@ apt-get install apache2-utils
 
 #docker
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common docker-ce -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common apache2-utils
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce -y
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.14.2/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
